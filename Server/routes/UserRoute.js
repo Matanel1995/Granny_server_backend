9c20ce3cc2c9b67e48de76931e1aac77e1f505d9
Matanel1995 <matanel5555@gmail.com>
2023-08-14 12:50:08 +0300

    fix issues

--- Server/routes/UserRoute.js
+++ Server/routes/UserRoute.js
@@ -1,35 +1,23 @@
 const express = require('express')
 const router = express.Router();
-const {db} = require('../FireBaseConfig');
-
-
-
-
-
-
+const { db } = require('../FireBaseConfig');
 
 // Get a reference to the document
 const collectionRef = db.collection('User')
 
 
 
-
-
 //--------------------GET METHODS ----------------------
-
 //GET details of specific user - using the userId. 
 //PATH = /user/:userId example - http://localhost:8080/user/userId?userId=118336558897872773954
 router.get('/userId', async (req, res) => {
-
     const docRef = collectionRef.doc(req.query.userId);
-
     // Get the document
     await docRef.get()
         .then((doc) => {
             if (doc.exists) {
                 let responseArr = [];
                 responseArr.push(doc.data());
-                console.log(responseArr[0].groupList);
                 res.status(200).json(responseArr);
             } else {
                 res.status(404).json('No such user!')
@@ -41,36 +29,23 @@ router.get('/userId', async (req, res) => {
 })
 
 
-
 // remove group from user group list http://localhost:8080/user/removeFromGroup?userId=103875074505523049381&groupId=Irfn1eWkyzenuWIuG8DK
-router.get('/removeFromGroup', async (req, res)=>{
-    // console.log("im heree");
+router.post('/removeFromGroup', async (req, res) => {
     const userId = req.query.userId;
     const groupId = req.query.groupId;
-    console.log(userId);
-    console.log(groupId);
-    console.log("finished printing infomation");
-
     const docRef = collectionRef.doc(userId);
-
-
     // Get the document 
     const userDoc = await docRef.get();
-    if(userDoc.exists){
+    if (userDoc.exists) {
         // get the group list from the response
         group_list = userDoc.data().groupList;
         // now check if the group Id is in the list
-        // print length before 
-        console.log(group_list.length);
-        try{
+        try {
             group_list = group_list.filter(id => id !== groupId);
-            // print length after
-            console.log(group_list.length);
-            console.log(group_list);
             // update the group list 
-            await docRef.update({ groupList: group_list});
+            await docRef.update({ groupList: group_list });
         }
-        catch (error){
+        catch (error) {
             // print error message
             console.log("Error:", error)
             res.status(404).json('No such groupId!')
@@ -83,30 +58,55 @@ router.get('/removeFromGroup', async (req, res)=>{
 
 
 // get group list from user ID http://localhost:8080/user/getGroups?userId=103875074505523049381
-router.get('/getGroups', async (req, res)=>{
-    // console.log("im heree");
+router.get('/getGroups', async (req, res) => {
     const userId = req.query.userId;
-    // console.log(userId);
-    // // console.log("finished printing infomation");
-
     const docRef = collectionRef.doc(userId);
-
     // Get the document 
     const userDoc = await docRef.get();
-    if(userDoc.exists){
+    if (userDoc.exists) {
         // get the group list from the response
         group_list = userDoc.data().groupList;
-        console.log(group_list);
         // print the result of the group list 
         res.status(200).json(group_list);
-    }  
+    }
     else {
         res.status(404).json('No such groupId!')
     }
 })
 
+router.post('/leaveGroup', async (req, res) => {
+    try {
+        const userId = req.body.userId;
+        const groupId = req.body.groupId;
+
+        const docRef = collectionRef.doc(userId);
+        const userDoc = await docRef.get();
+
+        if (userDoc.exists) {
+            const userData = userDoc.data();
+            const groupList = parseGroupList(userData.groupList.toString());
+
+            if (groupList.includes(groupId)) {
+                groupList.splice(groupList.indexOf(groupId), 1);
+                await docRef.update({ groupList: groupList });
+                res.status(200).json('Successfully left the group');
+            } else {
+                res.status(400).json('You are not a member of this group');
+            }
+        } else {
+            res.status(404).json('No such user!');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json('An error occurred');
+    }
+});
 
 
+// Parse the group list string into an array
+function parseGroupList(groupListString) {
+    return groupListString.split(',');
+}
 
 
 router.get('', async (req, res) => {
@@ -114,28 +114,6 @@ router.get('', async (req, res) => {
     return "working"
 }
 )
-
-
-/*
-משתמש - 
-
-    GET -   *הבאת משתמשים (מחזירה רשימה של משתמשים)(יחיד \ רבים).
-                 *הבאת הקבוצות (מחזריה רשימה של הקבוצות של אותו משתמש) (יחיד \ רבים).
-
-    POST-  *הוספה של יוזר לקבוצה כאשר הוא פותח אותה (עדכון) (אולי אפשר להשתמש בפונקציה שבPUT).
-    	
-    PUT- *הוספה של יוזר לקבוצה (עדכון).
-              *יציאה מקבוצה (עדכון \ אם אחרון בקבוצה מחיקה של הקבוצה).
-*/
-
-
-//--------------------POST METHODS ----------------------
-
-
-//--------------------DELETE METHODS ----------------------
-
-
-//--------------------PUT METHODS ----------------------
 
 
 
--- Server/routes/groupRoute.js
+++ Server/routes/groupRoute.js
@@ -1,22 +1,162 @@
 const express = require('express');
 const router = express.Router();
-const {db} = require('../FireBaseConfig');
+const { db } = require('../FireBaseConfig');
 
 
+// Get a reference to the document
+const collectionRef = db.collection('Group')
 
 
+// Define the route to create a new group
+router.post('/createGroup', async (req, res) => {
+    try {
+        const userId = req.body.userId;
+        const groupName = req.body.groupName;
+        const groupPhotoUrl =
+            req.body.groupPhotoUrl ||
+            'https://st4.depositphotos.com/11634452/41441/v/600/depositphotos_414416674-stock-illustration-picture-profile-icon-male-icon.jpg';
 
+        const docRef = collectionRef.doc();
+        const groupId = docRef.id;
+        const groupUsers = { [userId]: 0 };
+        //creating new group
+        await collectionRef.doc(groupId).set({
+            groupName: groupName,
+            groupId: groupId,
+            groupPhotoUrl: groupPhotoUrl,
+            groupUsers: groupUsers,
+            groupManagerId: userId
+        });
+        //update user groups list
+        await userRef.doc(userId).update({
+            groupList: admin.firestore.FieldValue.arrayUnion(groupId)
+        });
 
+        const createdGroup = {
+            groupId: groupId,
+            groupName: groupName,
+            groupPhotoUrl: groupPhotoUrl,
+            groupUsers: groupUsers,
+            groupManagerId: userId
+        };
+        res.status(200).json(createdGroup);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json('An error occurred');
+    }
+});
 
+// Define the route to add a user to an existing group (admin only)
+router.post('/addUserToGroup', async (req, res) => {
+    try {
+        const groupManagerId = req.body.groupManagerId;
+        const currentUserId = req.body.currentUser;
+        const groupId = req.body.groupId;
+        const userToAddId = req.body.userToAdd;
+        //check if group admin
+        if (groupManagerId === currentUserId) {
+            const docSnapshot = await collectionRef.doc(groupId).get();
 
+            if (docSnapshot.exists) {
+                const data = docSnapshot.data();
+                const groupUsers = data.groupUsers;
 
+                if (!groupUsers.hasOwnProperty(userToAddId)) {
+                    groupUsers[userToAddId] = 0;
+                }
 
+                await collectionRef.doc(groupId).update({ groupUsers: groupUsers });
 
+                // UPDATE HERE THE USER GROUPS LIST WITH THE USER METHOD!!!!!!!!!
+                await userRef.doc(userId).update({
+                    groupList: admin.firestore.FieldValue.arrayUnion(groupId)
+                });
 
+                res.status(200).json('User added to the group');
+            } else {
+                res.status(404).json('No such group');
+            }
+        } else {
+            // ADD HERE POP UP TO USER AND NOT JUST PRINT!!!!!!
+            res.status(403).json('You need to be an admin to add someone to the group');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json('An error occurred');
+    }
+});
 
+// Define the route to leave a group
+router.post('/leaveGroup', async (req, res) => {
+    try {
+        const userId = req.body.user;
+        const groupId = req.body.groupId;
 
+        let usersMap = {};
 
+        const docSnapshot = await collectionRef.doc(groupId).get();
+        if (docSnapshot.exists) {
+            const data = docSnapshot.data();
+            usersMap = parseMap(data.groupUsers.toString());
 
+            if (usersMap.hasOwnProperty(userId)) {
+                delete usersMap[userId];
+
+                await collectionRef.doc(groupId).update({ groupUsers: usersMap });
+                //call user leave function
+                try {
+                    const response = await axios.post('http://localhost:8080/user/leaveGroup', {
+                      userId: userId,
+                      groupId: groupId
+                    });
+                }
+                catch(err){
+                    console.log("Error: " + err);
+                }
+
+                res.status(200).json('Left the group successfully');
+            } else {
+                res.status(400).json('You are not a member of this group');
+            }
+        } else {
+            res.status(404).json('No such group');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json('An error occurred');
+    }
+});
+
+// Define the route to kick someone from a group (admin only)
+router.post('/kickFromGroup', async (req, res) => {
+    try {
+        const groupManagerId = req.body.groupManagerId;
+        const currentUserId = req.body.currentUser;
+        const userToKick = req.body.userToKick;
+        const groupId = req.body.groupId;
+
+        if (groupManagerId === currentUserId) {
+            //call user leave function to kick him
+            try {
+                const response = await axios.post('http://localhost:8080/user/leaveGroup', {
+                  userId: userToKick,
+                  groupId: groupId
+                });
+            }
+            catch(err){
+                console.log("Error: " + err);
+            }
+
+            res.status(200).json('User kicked from the group');
+        } else {
+            // ADD HERE POP UP TO USER AND NOT JUST PRINT!!!!
+            res.status(403).json('You need to be an admin to kick someone from the group');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json('An error occurred');
+    }
+});
 
 
 module.exports = router;
